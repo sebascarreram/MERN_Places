9d4365b70c28612f3d1f25292320e0f383cd7d96
Sebastian Carrera <sebascarreram@hotmail.com>
2020-01-30 16:12:00 -0500

    Created a Custom Form Hook (useForm)

--- front-end/src/shared/hooks/form-hook.js
+++ front-end/src/shared/hooks/form-hook.js
@@ -1,1 +1,49 @@
+/* eslint-disable */
+
 import { useCallback, useReducer } from "react";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid
+          }
+        },
+        isValid: formIsValid
+      };
+    case "":
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userForm = (initialInputs, initialFormValidity) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValidity
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id
+    });
+  }, []);
+  return [formState, inputHandler];
+};
