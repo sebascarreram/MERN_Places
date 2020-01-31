bbe17ecb2f81346f1212c41bd66c2c8bd3fc3229
Sebastian Carrera <sebascarreram@hotmail.com>
2020-01-30 19:08:42 -0500

    Added Auth

---
+++ front-end/src/user/pages/Auth.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
+import { userForm } from "../../shared/hooks/form-hook";
+
+import "./Auth.css";
+
+const Auth = props => {
+  //   useEffect(() => {
+  const [formState, inputHandler] = userForm(
+    {
+      email: {
+        value: "",
+        isValid: false
+      },
+      password: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
+  //   }, []);
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid E-Email"
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password, at least 5 characters"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
