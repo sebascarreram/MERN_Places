ed371f9d87d12507cd50b1da6c2d4f66c67fe74e
Sebastian Carrera <sebascarreram@hotmail.com>
2020-01-31 06:03:50 -0500

    Imported AuthContext for isLoggedIn, login & logout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useCallback } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 ////////////////////////////////////////////////////////////////
@@ -9,50 +10,64 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-import React from "react";
 // import "./App.css";
 
 const App = props => {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main>
-        <Switch>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          {/* //////////////////////////////// */}
-          {/* //////////////////////////////// */}
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </BrowserRouter>
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Switch>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+            <Route path="/:userId/places" exact>
+              <UserPlaces />
+            </Route>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+            <Route path="/places/new" exact>
+              <NewPlace />
+            </Route>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+            <Route path="/places/:placeId">
+              <UpdatePlace />
+            </Route>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            {/* //////////////////////////////// */}
+            {/* //////////////////////////////// */}
+
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
