4b1910107d376c1f6e0be32378a4d077630113dc
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-02 17:34:14 -0500

    Created Places & Adding it to a User

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -1,9 +1,11 @@
 // ...rest of the initial code omitted for simplicity.
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 const uuid = require("uuid/v4");
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../utils/location");
 const Place = require("../models/place");
+const User = require("../models/user");
 
 let DUMMY_PLACES = [
   {
@@ -117,8 +119,31 @@ const createPlace = async (req, res, next) => {
     creator
   });
 
+  let user;
   try {
-    await createdPlace.save();
+    user = await User.findById(creator);
+  } catch (err) {
+    const error = new HttpError("Creating place failed, please try again", 500);
+    return next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError("Could not find user for provided id", 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    ////////
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
+
+    ////////
   } catch (err) {
     const error = new HttpError("Creating place failed, please try again", 500);
     return next(error);
