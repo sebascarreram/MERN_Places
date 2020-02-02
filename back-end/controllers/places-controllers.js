abbaad7dba33a3ba80a007eafe145a741bda2818
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-01 20:16:32 -0500

    Location

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -2,6 +2,7 @@
 const { validationResult } = require("express-validator");
 const uuid = require("uuid/v4");
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../utils/location");
 
 let DUMMY_PLACES = [
   {
@@ -67,16 +68,25 @@ const getPlacesByUserId = (req, res, next) => {
 /////////////// Create Place
 ////////////////////////////////////////////////////////////////
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
   // const title = req.body.title
+  let coordinates;
+
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
   const createdPlace = {
     id: uuid(),
     title,
@@ -99,6 +109,12 @@ const createPlace = (req, res, next) => {
 ////////////////////////////////////////////////////////////////
 
 const updatePlaceById = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -118,6 +134,10 @@ const updatePlaceById = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find(place => place.id === placeId)) {
+    throw new HttpError("Could not find a place for that id.", 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId);
 
   res.status(200).json({
