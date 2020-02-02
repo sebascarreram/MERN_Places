9ad6d634f87afa05f1a46eba633dd861e8610a2c
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-02 11:44:57 -0500

    fixed

--- back-end/models/place.js
+++ back-end/models/place.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  imageURL: { type: String, required: true },
+  image: { type: String, required: true },
   address: { type: String, required: true },
   location: {
     lat: { type: Number, required: true },
