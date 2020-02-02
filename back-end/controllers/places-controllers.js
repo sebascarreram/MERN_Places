f9b5d346ee0551b7b5b58719f70d0364011accd8
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-02 18:28:38 -0500

    Modified -> deleting Places & Removing it From the User

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -210,7 +210,7 @@ const deletePlace = async (req, res, next) => {
 
   let place;
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate("creator");
   } catch (err) {
     const error = new HttpError(
       "Something went wrong, could not delete place",
@@ -219,8 +219,20 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (!place) {
+    const error = new HttpError("Could not find place for this id", 404);
+    return next(error);
+  }
+
   try {
-    await place.remove();
+    ///////////////
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
+    ///////////////
   } catch (err) {
     const error = new HttpError(
       "Something went wrong, could not delete place",
