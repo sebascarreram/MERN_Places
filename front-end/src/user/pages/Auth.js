61f6c8d5818b4f7e95a6c295e6bffb2eee92bef6
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-03 08:26:29 -0500

    Added LoadingSpinner for after created in signup and added ErrorModal UI

--- front-end/src/user/pages/Auth.js
+++ front-end/src/user/pages/Auth.js
@@ -3,6 +3,8 @@ import React, { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -17,6 +19,8 @@ const Auth = props => {
   const auth = useContext(AuthContext);
 
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = userForm(
     {
@@ -38,6 +42,7 @@ const Auth = props => {
     if (isLoginMode) {
     } else {
       try {
+        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -52,12 +57,16 @@ const Auth = props => {
 
         const responseData = await response.json();
         console.log(responseData);
+        ////
+        setIsLoading(false);
+        auth.login();
+        ////
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again");
       }
     }
-
-    auth.login();
   };
 
   const switchModelHandler = () => {
@@ -86,6 +95,7 @@ const Auth = props => {
 
   return (
     <Card className="authentication">
+      {isLoading && <LoadingSpinner asOverlay />}
       <h2>Login required</h2>
       <hr />
       <form onSubmit={authSubmitHandler}>
