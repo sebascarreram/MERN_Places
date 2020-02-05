2a215f36899908dfe5fc71d7ee575829497ef82e
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-04 19:53:43 -0500

    Serving Images Statically

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +12,8 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 // CORS Headers => Required for cross-origin/ cross-server communication
 app.use((req, res, next) => {
