6aa309805f110430c4b7dd75f72eafaaf8388c7f
Sebastian Carrera <sebascarreram@hotmail.com>
2020-02-05 16:43:27 -0500

    Authorization on 'Delete Places'

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -206,6 +206,16 @@ const deletePlace = async (req, res, next) => {
     const error = new HttpError("Could not find place for this id", 404);
     return next(error);
   }
+  ///////////////
+  ///////////////
+  ///////////////
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      "You are not allowed to delete this place.",
+      401
+    );
+    return next(error);
+  }
 
   const imagePath = place.image;
 
